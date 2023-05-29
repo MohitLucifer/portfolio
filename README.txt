4e813280f19e21e8635bd7cbc85ed61c12970980
MOHIT KUMAR <100012404+MohitLucifer@users.noreply.github.com>
2023-05-29 19:48:56 +0530

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,4 @@
+https://docs.google.com/spreadsheets/d/1ChRa35g6oZx8U_5EwIev7HeRhnxBRMHRvatDdC8EFcI/edit#gid=0
+
+sheet url
+ i 
---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+const nodemailer = require('nodemailer');
+var smtpTransport = require('nodemailer-smtp-transport')
+
+const app = express();
+console.log(__dirname);
+app.use(express.static(__dirname));// this we get all our static content to be visible on our website 
+app.use(bodyparser.urlencoded({ extended: true }));
+
+
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html");
+    console.log(__dirname);
+})
+
+app.post("/", function (req, res) {
+    const comm = req.body.Message;
+    const nme = req.body.Name;
+    var transporter = nodemailer.createTransport(smtpTransport({
+        service: 'gamil',
+        host: 'smtp.gmail.com',
+        port: 465,
+        auth: {
+            user: 'mohit8737kumar@gmail.com',
+            pass: 'ekomgrhjfoaqybty'
+        }
+    }));
+    var mailOptions = {
+        from: 'mohit8737kumar@gmail.com',
+        to: req.body.email,
+        cc: 'mohit8737kumar@gmail.com',
+        subject: 'Thanks for giving feedback ' + nme,
+        text: 'Thanks for your email. You have sent to us -->' + comm
+    };
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+        }
+        else {
+            res.redirect("/");
+            console.log("email sent" + info.response);
+        }
+    })
+});
+
+app.listen(3000, function () {
+    console.log("server started ");
+})
